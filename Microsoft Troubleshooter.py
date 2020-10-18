d91f848bf7d21e420830f517e4ed4aa89214fbb9
Payload9 <dreamyluigi360@gmail.com>
2020-10-18 13:34:57 -0600

    the first and only commit bc who needs updates

---
+++ Microsoft Troubleshooter.py
@@ -0,0 +1,9 @@
+import time
+import os
+
+os.system('cls')
+print("Scanning for problems and applying fixes.")
+# This here, my friends, totally runs a complete scan on your Windows 10 machine. It scans and tests every file, every nook and cranny, and every driver on your machine. Then, using Pythonic magic, the script applies completely accurate, Microsoft-verified fixes to any problems it finds. This totally legit process take under 60 seconds and I, Payload9, guarantee that your computer will be running like new after running this script.
+time.sleep(60)
+print("No problems found. Thank you for using the Microsoft Troubleshooter.")
+exit()
